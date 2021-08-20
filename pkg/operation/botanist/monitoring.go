dc4a8cc81547bdfdd57692306578938c6d767c44
Tim Usner <tim.usner@sap.com>
2021-08-20 10:07:48 +0200

    Handle Ingress networking.k8s.io/v1

--- pkg/operation/botanist/monitoring.go
+++ pkg/operation/botanist/monitoring.go
@@ -527,6 +527,12 @@ func (b *Botanist) DeleteSeedMonitoring(ctx context.Context) error {
 				Name:      "prometheus",
 			},
 		},
+		&networkingv1.Ingress{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: b.Shoot.SeedNamespace,
+				Name:      "prometheus",
+			},
+		},
 		&autoscalingv1beta2.VerticalPodAutoscaler{
 			ObjectMeta: metav1.ObjectMeta{
 				Namespace: b.Shoot.SeedNamespace,
